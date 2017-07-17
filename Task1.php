c0d0e7a6d38afc472c9bf16640b759c05c2e51f3
Natalia <Natalia.Vasilets@gmail.com>
2017-07-17 18:33:37 +0300

    First

---
+++ Task1.php
@@ -0,0 +1,30 @@
+      <?php
+  $a="php";
+  echo strtoupper($a);
+  ?>
+  <br>
+      <?php
+  $c='PHP';
+  echo strtolower($c);
+  ?>
+  <br>
+      <?php
+  $b="london";
+  echo ucfirst($b);
+  ?>
+  <br>
+      <?php
+  $b="london";
+  echo lcfirst($b);
+  ?>
+  <br>
+      <?php
+  $d='london is the capital of great britain';
+  echo ucwords($d);
+  ?>
+  <br>
+      <?php
+  $a='London';
+  $a2=strtolower($a);
+  echo ucfirst($a2);
+  ?>
